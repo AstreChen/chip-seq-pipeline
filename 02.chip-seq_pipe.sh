dcba2953c0c5a9f6109de39594584d46d5c73fdf
Chen Ai <chen@hulab.net>
2017-12-08 16:40:57 +0800

    add choose genome

--- 02.chip-seq_pipe.sh
+++ 02.chip-seq_pipe.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
-WORKDIR=/home/workdir/chen/proj/ighregion/embo2012/
+WORKDIR=/home/workdir/chen/proj/ighregion/geo40173
 LOG_DIR=$WORKDIR'/logs/'
 DATA_DIR=$WORKDIR'/data/'
+raw_dir=$DATA_DIR/2.fastq
 fastqc_dir=$DATA_DIR/fastqc
 trimmed_dir=$DATA_DIR/trim
+aligndir=$DATA_DIR/3.aligns
+peakdir=$DATA_DIR/4.peaks
 
+for folder in ($LOG_DIR,$fastqc_dir,$trimmed_dir,$aligndir,$peakdir)
+    if [! -x "$folder"]; then
+        mkdir "$folder"
+    fi
+
+SRRs=`cut -f1 /home/workdir/chen/proj/ighregion/geo40173/data/1.sample_info/p300_c-Myc_ctrl_info.csv`
 ###================== Preprocess befor Alignment ====================#####
 ##### fastqc #####
-#for fname in  $DATA_DIR/*.fastq.gz ; do
-#    fastqc $fname -o $fastqc_dir
-#done
+for fname in  $raw_dir/*.fastq.gz ; do
+    fastqc $fname -o $fastqc_dir
+done
 
 ### cut adapter and trim low quality reads ###
-#for sname in `grep 'SRR' $WORKDIR/sample_SRR_info.txt | cut -f2` ; do
+#for sname in $SRRs; do
 #    cutadapt -a 'GATCGGAAGAGCTCGTATGCCGTCTTCTGCTT' -g 'GATCGGAAGAGCTCGTATGCCGTCTTCTGCTT' -o $trimmed_dir/$sname'.trimmed.fastq.gz' $DATA_DIR/$sname'.fastq.gz' 1>> $LOG_DIR/cutadapt.trim.logs 2>> $LOG_DIR/cutadapt.trim.errors
-#    sickle se -f $trimmed_dir/$sname'.trimmed.fastq.gz' -t sanger -n -g -o $trimmed_dir/$sname'.trimmed2.fastq.gz' 1>> $LOG_DIR/sickle.trimmed.logs
+#    sickle se -f $raw_dir/$sname'.trimmed.fastq.gz' -t sanger -n -g -o $trimmed_dir/$sname'.trimmed2.fastq.gz' 1>> $LOG_DIR/sickle.trimmed.logs
 #done
 
 ### fastqc after filter reads ###
@@ -25,7 +34,6 @@ trimmed_dir=$DATA_DIR/trim
 ### ==============  Alignment  ===================== ###
 refdir='/home/workdir/chen/genomes/mm10/'
 idxdir=$refdir'/mm10_bowtie2_idx/'
-aligndir=$DATA_DIR/03.aligns
 #for sname in `grep 'SRR' $WORKDIR/sample_SRR_info.txt | cut -f2` ; do
 #    bowtie2 --local -N 0 -L 18 -x $idxdir/mm10 -U $trimmed_dir/$sname.trimmed2.fastq.gz -S $aligndir/$sname.sam 1>> $LOG_DIR/bowtie2.aligns.logs 2>> $LOG_DIR/bowtie2.aligns.errors
 #    echo "###"$sname"\n"
@@ -34,11 +42,13 @@ aligndir=$DATA_DIR/03.aligns
 #    samtools index $aligndir/$sname.sorted.bam $aligndir/$sname.idx
 #done
 
-peakdir=$DATA_DIR/04.peaks
-cd $aligndir
-macs2 callpeak -t \
-    SRR499700.sorted.bam  SRR499702.sorted.bam  SRR499704.sorted.bam  SRR499706.sorted.bam \
-    SRR499701.sorted.bam  SRR499703.sorted.bam  SRR499705.sorted.bam \
-    -c SRR499707.sorted.bam SRR499708.sorted.bam \
-    -f BAM -g mm --outdir $peakdir -n embo2012 -B -q 0.01 1>>$LOG_DIR/macs2.q01.logs  2>>$LOG_DIR/macs2.errors
-    
+#cd $aligndir
+#macs2 callpeak -t \
+#    -c SRR499707.sorted.bam SRR499708.sorted.bam \
+#    --SPMR --keep-dup 1 --nomodel \
+#    -f BAM -g mm --outdir $peakdir -n pax5 -B -q 0.01 1>>$LOG_DIR/macs2.1120.logs  2>>$LOG_DIR/macs2.1120.errors
+
+#macs2 bdgcmp -t embo2012_treat_pileup.bdg -c embo2012_control_lambda.bdg -o pax5_FE.bdg -m FE
+#LC_COLLATE=C sort -k1,1 -k2,2n pax5_FE.bdg > pax5_FE_sorted.bdg
+#/home/workdir/chen/tools/bedGraphToBigWig pax5_FE_sorted.bdg /home/workdir/chen/genomes/mm10/chromInfo.txt.gz pax5_FE.bwig
+
--- 02.chipseq.py
+++ 02.chipseq.py
@@ -2,20 +2,26 @@
 import pandas as pd
 import os,sys,subprocess
 import argparse
+import logging
+
+logger = logging.getLogger('root')
+logger.propagate = False
+
 
 parser = argparse.ArgumentParser(description='chipseq pipeline')
 parser.add_argument('-c','--ctrl', nargs='*', help='input the ctrl sample name, e.g.: -c NAME, represents NAME.fastq.gz')
 parser.add_argument('-t','--treat',nargs='*', help='input the case sample name, e.g.: -t NAME, represents NAME.fastq.gz')
 parser.add_argument('-f','--factor',help='input the chip-seq antibody name.')
 parser.add_argument('-r','--workdir',help='input the work dir, in this dir, a \'data/2.fastq/\' directory has been built for containing fastq files. please input the absolute path')
-
+parser.add_argument('-g','--genome',help='input the reference genome, "hg19" or "mm10", e.g. -g hg19. ')
 args = parser.parse_args()
-treat = [x for x in args.treat]
-ctrl = [x for x in args.ctrl]
+
 
 #smpinfo = '/home/workdir/chen/proj/ighregion/geo40173/data/1.sample_info/p300_c-Myc_ctrl_info.csv'
-refdir='/home/workdir/chen/genomes/mm10/mm10_bowtie2_idx/'
-
+#refdir='/home/workdir/chen/genomes/mm10/mm10_bowtie2_idx/'
+refdir='/home/ubuntu/genomes/bowtie2_indexes/'
+treat = [x for x in args.treat]
+ctrl = [x for x in args.ctrl]
 SRRs = ctrl+treat
 workdir = args.workdir +'/'
 #workdir = '/home/workdir/chen/proj/ighregion/geo40173/'
@@ -33,53 +39,74 @@ for folder in [logdir,rawdir,fqcdir,trimdir,aligndir,peakdir]:
 
 #SRRs = pd.read_csv(smpinfo,sep='\t',header=None).iloc[:,0].tolist()
 
-"""
 #### ===== FASTQC ====== #####
-for fname in os.listdir(rawdir) :
-    cmd = 'fastqc '+ rawdir + '/' + fname +' -o '+fqcdir
-    subprocess.call(cmd, shell=True)
+def fastqc(rawdir,fqcdir):
+    logger.info("Fastqc for quality control.")
+    for fname in os.listdir(rawdir) :
+        cmd = 'fastqc '+ rawdir + '/' + fname +' -o '+fqcdir
+        subprocess.call(cmd, shell=True)
+    logger.info("Finish Fastqc.")
 
 
 #### ==== trim low quality reads === ####
-for sname in SRRs:
-    print sname
-    cmd = 'sickle se -f '+ rawdir + sname+'.fastq.gz' + ' -t sanger -n -g -o ' + trimdir+sname+'.fastq.trimmed.gz ' \
-        + '1>> '+logdir+'sickle.trimmed.logs 2>>'+logdir+'sickle.trimmed.errors'
-    subprocess.call(cmd,shell=True)
+def trimReads(SRRs,rawdir,trimdir,logdir):
+    logger.info("Trimming low quality reads.")
+    for sname in SRRs:
+        print sname
+        cmd = 'sickle se -f '+ rawdir + sname+'.fastq.gz' + ' -t sanger -n -g -o ' + trimdir+sname+'.fastq.trimmed.gz ' \
+            + '1>> '+logdir+'sickle.trimmed.logs 2>>'+logdir+'sickle.trimmed.errors'
+        subprocess.call(cmd,shell=True)
+    logger.info("Finished trimming.")
 
 
 ### ==== FASTQC filter reads === ###
-for fname in os.listdir(trimdir):
-    cmd = 'fastqc '+ trimdir + '/' + fname +' -o '+fqcdir
-    subprocess.call(cmd,shell=True)
+def fqtrim(trimdir,fqcdir):
+    for fname in os.listdir(trimdir):
+        cmd = 'fastqc '+ trimdir + '/' + fname +' -o '+fqcdir
+        subprocess.call(cmd,shell=True)
 
 
 
 ### === Alignment === ###
-for sname in SRRs:
-    print sname
-    cmd = 'bowtie2 -N 1 -x '+ refdir+'mm10 -U '+ trimdir+sname+'.fastq.trimmed.gz ' \
-        + ' -S '+aligndir+sname+'.sam 1>> '+logdir+'bowtie2.aligns.logs 2>> '+ logdir+'bowtie2.aligns.errors ;' \
-        + ' samtools view -S -b -o '+aligndir+sname+'.bam '+ aligndir+sname+'.sam ;' \
-        + ' samtools sort '+aligndir+sname+'.bam '+aligndir+sname+'.sorted '
-    subprocess.call(cmd,shell=True)
-"""
+def alignReads(SRRs,refdir,args,trimdir,logdir,aligndir):
+    logger.info("Align reads...")
+    for sname in SRRs:
+        print sname
+        cmd = 'bowtie2 -N 1 -x '+ refdir+args.genome+' -U '+ trimdir+sname+'.fastq.trimmed.gz ' \
+            + ' -S '+aligndir+sname+'.sam 1>> '+logdir+'bowtie2.aligns.logs 2>> '+ logdir+'bowtie2.aligns.errors ;' \
+            + ' samtools view -S -b -o '+aligndir+sname+'.bam '+ aligndir+sname+'.sam ;' \
+            + ' samtools sort '+aligndir+sname+'.bam '+aligndir+sname+'.sorted '
+        subprocess.call(cmd,shell=True)
+    logger.info("Align reads complete.")
 
 ### === call peak === ###
-cmd = 'cd '+ aligndir + ' ; ' \
-    + 'macs2 callpeak -t '+ '.sorted.bam '.join(treat) +'.sorted.bam ' \
-    + ' -c '+ '.sorted.bam '.join(ctrl)+'.sorted.bam ' + ' --keep-dup 1 --nomodel -f BAM -g mm ' \
-    + '--outdir '+peakdir+' -n '+args.factor +' -B -q 0.01 1>> '+logdir+'macs2_' + args.factor +'.logs 2>>'+logdir+'macs2_'+args.factor+'.errors '
-print cmd
-subprocess.call(cmd, shell=True)
+def callPeak(args,aligndir,logdir,peakdir):
+    logger.info("Call ChIP peaks.")
+    treat = [x for x in args.treat]
+    ctrl = [x for x in args.ctrl]
+    cmd = 'cd '+ aligndir + ' ; ' \
+        + 'macs2 callpeak -t '+ '.sorted.bam '.join(treat) +'.sorted.bam ' \
+        + ' -c '+ '.sorted.bam '.join(ctrl)+'.sorted.bam ' + ' --keep-dup 1 --nomodel -f BAM -g mm ' \
+        + '--outdir '+peakdir+' -n '+args.factor +' -B -q 0.01 1>> '+logdir+'macs2_' + args.factor +'.logs 2>>'+logdir+'macs2_'+args.factor+'.errors '
+    print cmd
+    subprocess.call(cmd, shell=True)
+    cmd = ' cd '+peakdir + '; LC_COLLATE=C sort -k1,1 -k2,2n '+ args.factor +'_treat_pileup.bdg > '+ args.factor +'_treat_pileup_sorted.bdg ;' \
+    + '/home/workdir/chen/tools/bedGraphToBigWig '+ args.factor +'_treat_pileup_sorted.bdg /home/workdir/chen/genomes/mm10/chromInfo.txt.gz '+ args.factor +'.bw '
+    print cmd
+    subprocess.call(cmd, shell=True)
+
+    cmd = ' cd '+peakdir + '; LC_COLLATE=C sort -k1,1 -k2,2n '+ args.factor +'_control_lambda.bdg > '+ args.factor +'_control_lambda_sorted.bdg ;' \
+        + '/home/workdir/chen/tools/bedGraphToBigWig '+ args.factor +'_control_lambda_sorted.bdg /home/workdir/chen/genomes/mm10/chromInfo.txt.gz '+ args.factor +'_ctrl.bw '
+    print cmd
+    subprocess.call(cmd, shell=True)
+    logger.info("Finished call peaks.")
+
+if __name__=="__main__":
+    fastqc(rawdir,fqcdir)
+    trimReads(SRRs,rawdir,trimdir,logdir)
+    fqtrim(trimdir,fqcdir)
+    alignReads(SRRs,refdir,args,trimdir,logdir,aligndir)
+    callPeak(args,aligndir,logdir,peakdir)
 
 
-cmd = ' cd '+peakdir + '; LC_COLLATE=C sort -k1,1 -k2,2n '+ args.factor +'_treat_pileup.bdg > '+ args.factor +'_treat_pileup_sorted.bdg ;' \
-+ '/home/workdir/chen/tools/bedGraphToBigWig '+ args.factor +'_treat_pileup_sorted.bdg /home/workdir/chen/genomes/mm10/chromInfo.txt.gz '+ args.factor +'.bw '
-print cmd
-subprocess.call(cmd, shell=True)
 
-cmd = ' cd '+peakdir + '; LC_COLLATE=C sort -k1,1 -k2,2n '+ args.factor +'_control_lambda.bdg > '+ args.factor +'_control_lambda_sorted.bdg ;' \
-+ '/home/workdir/chen/tools/bedGraphToBigWig '+ args.factor +'_control_lambda_sorted.bdg /home/workdir/chen/genomes/mm10/chromInfo.txt.gz '+ args.factor +'_ctrl.bw '
-print cmd
-subprocess.call(cmd, shell=True)
